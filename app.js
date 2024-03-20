1a163f57e7d42a740ff31fb799238ccdfe4ca53c
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-03-20 17:23:28 +0700

    update 5

--- app.js
+++ app.js
@@ -6,9 +6,32 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-// index page
-app.get('/', function(req, res) {
-  res.render('../views/index', {title: "Home"});
+//  beranda
+app.get("/", (req, res) => {
+  res.render("index.ejs", {title: 'Beranda'});
+});
+
+// jelajah
+app.get("/novel", (req, res) => {
+  res.render("novel.ejs", {title: 'Buku Novel'});
+});
+
+app.get("/inspirasi", (req, res) => {
+  res.render("Inspirasi.ejs", {title: 'Buku Inspirasi'});
+});
+
+// contact
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs", {title: 'Hubungi Kami'});
+});
+
+// form
+app.get("/form", (req, res) => {
+  res.render("form.ejs", {title: 'Form Peminjaman'});
+});
+
+app.get("/sukses", (req, res) => {
+  res.render("sukses.ejs", {title: 'Sukses Meminjam'});
 });
 
 app.listen(8080);
