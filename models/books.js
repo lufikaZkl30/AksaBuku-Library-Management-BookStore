95664896bc054396da23f67c5510076f52a2d828
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-04-11 00:54:43 +0700

    add models

---
+++ models/books.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const bukuSchema = new mongoose.Schema({
+  judul: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  penerbit: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  sinopsis: {
+    type: String,
+    required: true
+  },
+  tahun: {
+    type: Number,
+    required: true
+  },
+  halaman: {
+    type: Number,
+    required: true
+  },
+  harga: {
+    type: Number,
+    required: true
+  },
+  stok: {
+    type: Number,
+    required: true
+  },
+  kategori: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Books', bukuSchema);
