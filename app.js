f6fc75b96e1e6f331934fc3f9be99bb2c998c504
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-04-27 02:31:18 +0700

    Update signup and modules
    Sudah bisa signup, belum ada passport.ejs untuk login

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const nodemailer = require('nodemailer');
 const randomstring = require('randomstring');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const multer = require('multer');
-const fs = require('fs');
+const flash = require('connect-flash');
+const passport = require('passport');
 //----------------
+
+//AUTH CONFIG
+const { ensureAuthenticated, forwardAuthenticated} = require("./config/auth");
 
 //APP
 var app = express();
@@ -18,11 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //----------------
 
-
-//ROUTER - BOOKS
-const Books = require('./models/books');
-const booksRouter = require('./routes/api/books');
-app.use('/api/books', booksRouter);
+//PASSPORT
+require('./config/passport')(passport);
 //----------------
 
 //VARIABEL
@@ -58,55 +58,107 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
-});
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+//----------------
+
+//FLASH MIDDLEWARE
+  app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+//----------------
+
+//ROUTER - BOOKS
+const Books = require('./models/books');
+const booksRouter = require('./routes/api/books');
+app.use('/api/books', booksRouter);
+//----------------
+
+//ROUTER - USERS
+const users = require('./models/users');
+const usersRouter = require('./routes/api/users');
+app.use('/', usersRouter);
 //----------------
 
 //PAGES
-/*Index*/
-app.get("/", async (req, res) => {
-  try{
-    const latestBooks = await Books.find().sort({ createdAt: 1 }).limit(3);
-    const discountBooks = await Books.find({ promo: "Enable" });
-    const popularBooks = await Books.aggregate([
-      { $sample: { size: 3 } }
-    ]);
-    res.render("index.ejs", {title: 'Beranda', books: latestBooks, popularBooks: popularBooks, discountBooks: discountBooks});
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  /*Index*/
+  app.get("/", async (req, res) => {
+    try{
+      const latestBooks = await Books.find().sort({ createdAt: 1 }).limit(3);
+      const discountBooks = await Books.find({ promo: "Enable" });
+      const popularBooks = await Books.aggregate([
+        { $sample: { size: 3 } }
+      ]);
+      res.render("index.ejs", {title: 'Beranda', books: latestBooks, popularBooks: popularBooks, discountBooks: discountBooks});
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  /*Buku-Buku*/
+  function handleKategori(app, kategori){
+    const judulKategori = {
+        'novel': 'Buku Novel',
+        'inspirasi': 'Buku Inspirasi',
+        'sejarah': 'Buku Sejarah',
+        'komik': 'Buku Komik',
+        'resepmasakan': 'Buku Resep & Masakan',
+        'bisnisekonomi': 'Buku Bisnis & Ekonomi',
+        'bahasaasing': 'Buku Bahasa Asing',
+        'medis': 'Buku Medis'
+    };
+
+    app.get(`/${kategori}`, async (req, res) => {
+      try {
+          const books = await Books.find({ kategori: kategori });
+          res.render(`buku/${kategori}Page`, { books, title: judulKategori[kategori] });
+      } catch (err) {
+          res.status(500).json({ message: err.message });
+      }
+    });
+
+    app.get(`/${kategori}/:id`, async (req, res) => {
+      try {
+          const book = await Books.findById(req.params.id);
+          if (!book || book.kategori.toLowerCase() !== kategori) {
+              return res.status(404).json({ message: 'Buku tidak ditemukan.' });
+          }
+          res.render('buku/detailBuku', { book, title: 'Detail Buku' });
+      } catch (err) {
+          res.status(500).json({ message: err.message });
+      }
+    });
   }
-});
 
-/*Buku-Buku*/
-function handleKategori(app, kategori){
-  const judulKategori = {
-      'novel': 'Buku Novel',
-      'inspirasi': 'Buku Inspirasi',
-      'sejarah': 'Buku Sejarah',
-      'komik': 'Buku Komik',
-      'resepmasakan': 'Buku Resep & Masakan',
-      'bisnisekonomi': 'Buku Bisnis & Ekonomi',
-      'bahasaasing': 'Buku Bahasa Asing',
-      'medis': 'Buku Medis'
-  };
+  handleKategori(app, 'novel');
+  handleKategori(app, 'inspirasi');
+  handleKategori(app, 'sejarah');
+  handleKategori(app, 'komik');
+  handleKategori(app, 'resepmasakan');
+  handleKategori(app, 'bisnisekonomi');
+  handleKategori(app, 'bahasaasing');
+  handleKategori(app, 'medis');
 
-  app.get(`/${kategori}`, async (req, res) => {
+
+  /*Pinjam Buku*/
+  app.get("/rentbook", async (req, res) => {
     try {
-        const books = await Books.find({ kategori: kategori });
-        res.render(`buku/${kategori}Page`, { books, title: judulKategori[kategori] });
+        const books = await Books.find({ rentbook: "Enable" });
+        res.render("rentbook.ejs", { books, title: "Peminjaman Buku" });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
   });
 
-  app.get(`/${kategori}/:id`, async (req, res) => {
+  app.get('/rentbook/:id', async (req, res) => {
     try {
         const book = await Books.findById(req.params.id);
-        if (!book || book.kategori.toLowerCase() !== kategori) {
+        if (!book) {
             return res.status(404).json({ message: 'Buku tidak ditemukan.' });
         }
         res.render('buku/detailBuku', { book, title: 'Detail Buku' });
@@ -114,171 +166,138 @@ function handleKategori(app, kategori){
         res.status(500).json({ message: err.message });
     }
   });
-}
 
-handleKategori(app, 'novel');
-handleKategori(app, 'inspirasi');
-handleKategori(app, 'sejarah');
-handleKategori(app, 'komik');
-handleKategori(app, 'resepmasakan');
-handleKategori(app, 'bisnisekonomi');
-handleKategori(app, 'bahasaasing');
-handleKategori(app, 'medis');
-
-
-/*Pinjam Buku*/
-app.get("/rentbook", async (req, res) => {
-  try {
-      const books = await Books.find({ rentbook: "Enable" });
-      res.render("rentbook.ejs", { books, title: "Peminjaman Buku" });
-  } catch (err) {
-      res.status(500).json({ message: err.message });
-  }
-});
-
-app.get('/rentbook/:id', async (req, res) => {
-  try {
-      const book = await Books.findById(req.params.id);
-      if (!book) {
-          return res.status(404).json({ message: 'Buku tidak ditemukan.' });
-      }
-      res.render('buku/detailBuku', { book, title: 'Detail Buku' });
-  } catch (err) {
-      res.status(500).json({ message: err.message });
-  }
-});
-
-app.get('/readbook/:id', async (req, res) => {
-  try {
-      const book = await Books.findById(req.params.id);
-      if (!book) {
-          return res.status(404).json({ message: 'Buku tidak ditemukan.' });
-      }
-      res.render('readbook', { book, title: 'Baca Buku', pdfPath: book.pdfPath });
-  } catch (err) {
-      res.status(500).json({ message: err.message });
-  }
-});
-
-
-/*Login & Sign Up*/
-app.get("/login", (req, res) => {
-  res.render("login.ejs", {title: 'Login'});
-});
-
-app.get("/signup", (req, res) => {
-  res.render("signup.ejs", {title: 'Daftar'});
-});
-
-/*Contact & Form*/
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs", {title: 'Hubungi Kami', msgCode: messageCode});
-});
-
-app.get("/form", (req, res) => {
-  res.render("form.ejs", {title: 'Form Peminjaman'});
-});
-
-/*Admin Page*/
-app.get("/admin/addbooks", (req, res) => {
-  res.render("admin/addbooks.ejs", {title: 'Tambah Buku'});
-});
-
-app.get('/admin/booklists', async (req, res) => {
-  try {
-    const books = await Books.find({});
-    res.render('admin/booklists.ejs', { title: 'Daftar Buku', books: books });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-app.get('/admin/editbooks/:id', async (req, res) => {
-  try {
-    const book = await Books.findById(req.params.id);
-    if (!book) {
-      return res.status(404).json({ message: 'Buku tidak ditemukan' });
+  app.get('/readbook/:id', async (req, res) => {
+    try {
+        const book = await Books.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: 'Buku tidak ditemukan.' });
+        }
+        res.render('readbook', { book, title: 'Baca Buku', pdfPath: book.pdfPath });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    res.render('admin/editbooks.ejs', { title: 'Edit Buku', book: book });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+  });
 
-/*Payment*/
-app.get('/payment/:id', async (req, res) => {
-  try {
+
+  /*Login & Sign Up*/
+  app.get("/login", (req, res) => {
+    res.render("login.ejs", {title: 'Login'});
+  });
+
+  app.get("/signup", (req, res) => {
+    res.render("signup.ejs", {title: 'Daftar'});
+  });
+
+  /*Contact & Form*/
+  app.get("/contact", (req, res) => {
+    res.render("contact.ejs", {title: 'Hubungi Kami', msgCode: messageCode});
+  });
+
+  app.get("/form", (req, res) => {
+    res.render("form.ejs", {title: 'Form Peminjaman'});
+  });
+
+  /*Admin Page*/
+  app.get("/admin/addbooks", (req, res) => {
+    res.render("admin/addbooks.ejs", {title: 'Tambah Buku'});
+  });
+
+  app.get('/admin/booklists', async (req, res) => {
+    try {
+      const books = await Books.find({});
+      res.render('admin/booklists.ejs', { title: 'Daftar Buku', books: books });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+  app.get('/admin/editbooks/:id', async (req, res) => {
+    try {
       const book = await Books.findById(req.params.id);
       if (!book) {
-          return res.status(404).json({ message: 'Book not found' });
+        return res.status(404).json({ message: 'Buku tidak ditemukan' });
       }
-      res.render('paymentPage', {title: 'Payment', book});
-  } catch (err) {
+      res.render('admin/editbooks.ejs', { title: 'Edit Buku', book: book });
+    } catch (err) {
       res.status(500).json({ message: err.message });
-  }
-});
+    }
+  });
+
+  /*Payment*/
+  app.get('/payment/:id', async (req, res) => {
+    try {
+        const book = await Books.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.render('paymentPage', {title: 'Payment', book});
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+  });
 //----------------
 
 //POST REQUEST
-app.post('/contact', (req,res) => {
-  const output = `
-    <h3>Detail Kontak:</h3>
-    <p>Name: ${req.body.name}</p>
-    <p>Email: ${req.body.email}</p>
-    <h3>Message:</h3>
-    <p>${req.body.message}</p>
-    <hr>
-    <p>Nomor Referensi: ${messageCode}</p>
-  `;
+  app.post('/contact', (req,res) => {
+    const output = `
+      <h3>Detail Kontak:</h3>
+      <p>Name: ${req.body.name}</p>
+      <p>Email: ${req.body.email}</p>
+      <h3>Message:</h3>
+      <p>${req.body.message}</p>
+      <hr>
+      <p>Nomor Referensi: ${messageCode}</p>
+    `;
 
-  const thankyou = `
-    <p>Halo, ${req.body.name}</p>
-    <p>Nomor Referensi Anda: ${messageCode}</p>
-    <p>Terima kasih telah menghubungi tim AKSA BUKU.
-    Kami akan segera menghubungi Anda.</p><br>
-    <p>Salam,</p>
-    <p>Aksa Buku</p>
-  `;
+    const thankyou = `
+      <p>Halo, ${req.body.name}</p>
+      <p>Nomor Referensi Anda: ${messageCode}</p>
+      <p>Terima kasih telah menghubungi tim AKSA BUKU.
+      Kami akan segera menghubungi Anda.</p><br>
+      <p>Salam,</p>
+      <p>Aksa Buku</p>
+    `;
 
-  //Notification for new Message
-  let mailOptions = {
-    from: sender,
-    to: notifier,
-    subject: 'You have a new email request!',
-    html: output
-  };
+    //Notification for new Message
+    let mailOptions = {
+      from: sender,
+      to: notifier,
+      subject: 'You have a new email request!',
+      html: output
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }  
-      console.log('Message sent: %s' + info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-      res.render("contact.ejs", { title: 'Hubungi Kami'});
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }  
+        console.log('Message sent: %s' + info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.render("contact.ejs", { title: 'Hubungi Kami'});
+    });
 
-  //Thank You Email
-  let mailOptionsThankYou = {
-    from: sender,
-    to: `${req.body.email}`,
-    subject: 'Thank you for contacting us!',
-    html: thankyou
-  };
+    //Thank You Email
+    let mailOptionsThankYou = {
+      from: sender,
+      to: `${req.body.email}`,
+      subject: 'Thank you for contacting us!',
+      html: thankyou
+    };
 
-  transporter.sendMail(mailOptionsThankYou, (error, info) => {
-    if (error) {
-      console.log('Error sending thank you email:', error);
-    } else {
-      console.log('Thank you email sent:', info.messageId);
-    }
-  });
-  messageCode = randomstring.generate(8);
-  res.redirect('/contact');
-})
+    transporter.sendMail(mailOptionsThankYou, (error, info) => {
+      if (error) {
+        console.log('Error sending thank you email:', error);
+      } else {
+        console.log('Thank you email sent:', info.messageId);
+      }
+    });
+    messageCode = randomstring.generate(8);
+    res.redirect('/contact');
+  })
 
 
 //LISTEN TO PORT
-app.listen(port, ()=>{
-  console.log(`http://localhost:${port}`);
-});
+  app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
+  });
 //--------------
---
+++ routes/api/users.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
+const User = require("../../models/users");
+
+//sign up post
+router.post("/signup", (req, res) => {
+  const { name, uname, phone, email, password, password2 } = req.body;
+  let errors = [];
+
+  //Check required fields
+  if (!name || !uname || !phone || !email || !password || !password2) {
+    errors.push({ msg: "Harap menginput semua data" });
+  }
+
+  // Password match
+  if (password !== password2) {
+    errors.push({ msg: "Password tidak sama" });
+  }
+
+  //Check name length
+  if (name.length < 3 || name.length > 50) {
+    errors.push({ msg: "Nama minimal 3 karakter" });
+  }
+
+  //Check username length
+  if (uname.length < 3 || uname.length > 50) {
+    errors.push({ msg: "Username minimal 3 karakter" });
+  }
+
+  //Check pass length
+  if (password.length < 6 || password2.length < 6) {
+    errors.push({ msg: "Password minimal 6 karakter" });
+  }
+
+  //Check errors
+  if (errors.length > 0) {
+    res.render("signup", {
+      errors,
+      name,
+      uname,
+      phone,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        errors.push({ msg: "Email sudah terdaftar" });
+        res.render("signup", {
+          errors,
+          name,
+          uname,
+          phone,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          uname,
+          phone,
+          email,
+          password
+        });
+        //Hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            //Set password to hash
+            newUser.password = hash;
+
+            //Save user
+            newUser
+              .save()
+              .then((user) => {
+                req.flash("success_msg", "Anda berhasil registrasi, Silahkan Login");
+                res.redirect("/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
+});
+
+//Login Handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/index",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+//logout handle
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "Anda berhasil Log out");
+  res.redirect("/login");
+});
+
+module.exports = router;
